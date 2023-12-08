b0bf14af91bbadc2fde9918f2d7e00750c312a7e
angelHunther <angelmanuel02401@gmail.com>
2023-12-08 11:17:49 -0600

    puntos

--- GestionarClientes.php
+++ GestionarClientes.php
@@ -2,17 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Mogra&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <?php require_once 'assets/library/librerias.php' ?>
     <link rel="stylesheet" href="./css/clientesStyle.css">
     <title>GESTIONAR CLIENTES</title>
 </head>
@@ -21,15 +11,7 @@
     <!--se agrega como grid container por la clase de maquetado que se esta realizando -->
     <header class="header">
 
-        <div class="pages">
-            <h3>Páginas/Gestionar Clientes</h3>
-            <h2>Gestionar Clientes</h2>
-        </div>
-        <div class="icons">
-            <i class="bi bi-person"></i>
-            <i class="bi bi-gear"></i>
-            <i class="bi bi-bell"></i>
-        </div>
+        <?php require_once 'view/header.php' ?>
     </header>
     <nav class="navbar">
         <div class="search-bar">
--- view/sidebar.php
+++ view/sidebar.php
@@ -10,7 +10,7 @@
         <div class="registros">
             <p>---REGISTROS---</p>
             <form>
-                <a href="./GestionarClientes.html"> 
+                <a href="./GestionarClientes.php"> 
                         <i class="bi bi-pencil-square"></i>Gestionar Clientes
                 </a>
                 <a href="./GestionarMascota.html">
