a8aff90f1d417c0b81267eaee821de884e9ae57b
angelHunther <angelmanuel02401@gmail.com>
2023-12-08 11:10:40 -0600

    carpeta view

--- index.php
+++ index.php
@@ -1,82 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+<header>
 <?php require_once 'assets/library/librerias.php' ?>
+</header>
+
 <body class="grid-container">
     <!--se agrega como grid container por la clase de maquetado que se esta realizando -->
     <header class="header">
-
-        <div class="pages">
-            <h3>Páginas/Dashboard</h3>
-            <h2>Dashboard</h2>
-        </div>
-        <div class="icons">
-            <i class="bi bi-person"></i>
-            <i class="bi bi-gear"></i>
-            <i class="bi bi-bell"></i>
-        </div>
+        <?php require_once 'view/header.php' ?>
+        
     </header>
     <nav class="navbar">
-            <div class="text">
-                <h1>¡Hola de nuevo!</h1>
-                <h3>Fulanito</h3>
-            </div>
-            <div class="users">
-                <div class="icon"><i class="bi bi-person"></i></div>
-                <div class="cant"><h6>Cantidad usuarios</h6><h3>350</h3></div>
-            </div>
-            <div class="users">
-                <div class="icon"><i class="bi bi-person"></i></div>
-                <div class="cant"><h6>Cantidad usuarios</h6><h3>350</h3></div>
-            </div>
+            <?php require_once 'view/navbar.php'?>
     </nav>
     <aside class="sidebar">
-        <h2>Beraca</h2>
-        <div class="userside">
-            <div class="cube">
-                <i class="bi bi-person"></i>
-            </div>
-            <a href="./index.html">
-                <p>Dashboard</p>
-            </a>
-        </div>
-        <div class="registros">
-            <p>---REGISTROS---</p>
-            <form>
-                <a href="./GestionarClientes.html"> 
-                        <i class="bi bi-pencil-square"></i>Gestionar Clientes
-                </a>
-                <a href="./GestionarMascota.html">
-                    <i class=" bi bi-gear"></i>
-                    Gestionar Mascotas
-                </a>
-                <a href="#">
-                    <i class="bi bi-calendar2-heart"></i>
-                    Gestionar Consultas
-                </a>
-                <a  href="#">
-                    <i class="bi bi-calendar-check"></i>
-                    Gestionar Citas
-                </a>
-                <a type="submit" href="#">
-                    <i class="bi bi-journal-bookmark"></i>
-                    Gestionar Agenda
-                </a>
-                <a href="#">
-                    <i class="bi bi-chat-heart"></i>
-                    Gestionar Mensajes
-                </a>
-                <p>---BUSQUEDA---</p>
-                <a href="#">
-                    <i class="bi bi-search-heart"></i>
-                    Busqueda Adopción
-                </a>
-                <p>---OTROS---</p>
-                <a  href="#">
-                    <i class="bi bi-mortarboard"></i>
-                    ¿Sabías Qué?
-                </a>
-            </form>
-        </div>
+        <?php require_once 'view/sidebar.php'?>
     </aside>
     <article class="main">
         <div class="perfil">
---
+++ view/sidebar.php
@@ -0,0 +1,47 @@
+<h2>Beraca</h2>
+        <div class="userside">
+            <div class="cube">
+                <i class="bi bi-person"></i>
+            </div>
+            <a href="./index.html">
+                <p>Dashboard</p>
+            </a>
+        </div>
+        <div class="registros">
+            <p>---REGISTROS---</p>
+            <form>
+                <a href="./GestionarClientes.html"> 
+                        <i class="bi bi-pencil-square"></i>Gestionar Clientes
+                </a>
+                <a href="./GestionarMascota.html">
+                    <i class=" bi bi-gear"></i>
+                    Gestionar Mascotas
+                </a>
+                <a href="#">
+                    <i class="bi bi-calendar2-heart"></i>
+                    Gestionar Consultas
+                </a>
+                <a  href="#">
+                    <i class="bi bi-calendar-check"></i>
+                    Gestionar Citas
+                </a>
+                <a type="submit" href="#">
+                    <i class="bi bi-journal-bookmark"></i>
+                    Gestionar Agenda
+                </a>
+                <a href="#">
+                    <i class="bi bi-chat-heart"></i>
+                    Gestionar Mensajes
+                </a>
+                <p>---BUSQUEDA---</p>
+                <a href="#">
+                    <i class="bi bi-search-heart"></i>
+                    Busqueda Adopción
+                </a>
+                <p>---OTROS---</p>
+                <a  href="#">
+                    <i class="bi bi-mortarboard"></i>
+                    ¿Sabías Qué?
+                </a>
+            </form>
+        </div>
